8a562616ee19411da6d6d9246a08e8876f19bbe8
acp12395 <acp12395@gmail.com>
2025-10-19 14:11:42 +0900

    Project_Fractal: Applied hue

--- Learn_Advanced_C++_Programming/Project_Fractal/src/main.cpp
+++ Learn_Advanced_C++_Programming/Project_Fractal/src/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<cstdio>
+#include<memory>
+#include<math.h>
 #include"../header/Mandelbrot.h"
 #include"../header/Bitmap.h"
 
@@ -13,16 +15,43 @@ int main()
     const int HEIGHT = 600;
     Bitmap map(WIDTH,HEIGHT);
     string fileName{"image.bmp"};
+    unique_ptr<int[]> iterationsHistogram(new int[Mandelbrot::MAX_ITERATIONS]{0});
+    unique_ptr<int[]> fractalIterations(new int[WIDTH*HEIGHT]{0});
+    long total = 0;
     for(int x = 0; x < WIDTH; x++)
     {
         for(int y = 0; y < HEIGHT; y++)
         {
             double x_fractal = (x - WIDTH/2 - 200) * (2.0/HEIGHT);
             double y_fractal = (y - HEIGHT/2) * (2.0/HEIGHT);
-            uint8_t intensity = (uint8_t)((256*(double)(Mandelbrot::getIterations(x_fractal,y_fractal))/Mandelbrot::MAX_ITERATIONS));
-            map.setPixel(x,y,0,intensity,0);
+            int iterations = Mandelbrot::getIterations(x_fractal,y_fractal);
+            fractalIterations[y*WIDTH + x] = iterations;
+            if(iterations < Mandelbrot::MAX_ITERATIONS)
+            {
+                iterationsHistogram[iterations]++;
+                total++;
+            }
         }
     }
+
+    for(int x = 0; x < WIDTH; x++)
+    {
+        for(int y = 0; y < HEIGHT; y++)
+        {
+            int iterations = fractalIterations[y*WIDTH + x];
+            double hue = 0.0;
+            if(iterations < Mandelbrot::MAX_ITERATIONS)
+            {
+                for(int i = 0; i <= iterations; i++)
+                {
+                    hue += (static_cast<double>(iterationsHistogram[i]))/total;
+                }
+            }
+            map.setPixel(x,y,0,hue*255,0);
+        }
+    }
+    
+
     map.write(fileName);
     cout << fileName << " written" << endl;
 
